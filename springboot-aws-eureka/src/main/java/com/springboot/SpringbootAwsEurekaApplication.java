0cfd170317d5e3c8ff87d1a0b56ff3711606bb55
gopikrishhnachokkalamani <gopikrishhnachokkalamani@gopikrihhnasmbp>
2019-01-30 22:59:43 -0500

    test

---
+++ springboot-aws-eureka/src/main/java/com/springboot/SpringbootAwsEurekaApplication.java
@@ -0,0 +1,31 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Configuration
+public class SpringbootAwsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEurekaApplication.class, args);
+	}
+
+	@Bean
+	@Profile("!default")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+	  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+	  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	  b.setDataCenterInfo(info);
+	  return b;
+	}
+}
